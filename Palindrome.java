a3398808ab9b92ff7f215971844f87a640ee347c
arc1124 <architgupta737@gmail.com>
2022-06-14 18:43:09 +0530

    Program to find palindrome.

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        int size = A.length();
+        String R = "";
+        for (int i = size - 1; i >= 0; i -= 1) {
+            R = R + A.charAt(i);
+        }
+        if ((R.compareTo(A)) == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
