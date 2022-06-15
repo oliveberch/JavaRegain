c3abb5ccb16a0d06e4c4d6df5c62f4cf38db833c
arc1124 <architgupta737@gmail.com>
2022-06-16 00:54:08 +0530

    Programto Reverse Input String.

---
+++ RevStr.java
@@ -0,0 +1,42 @@
+/*
+CREATING CODES FOR REVERSING STRING WITHOUT USING INBUILT JDK LIBRARY FUNCTION OF charAt().
+HAVE TAKEN EACH AND EVERY CHARAACTER IN STRING IN ASCII CODE AS A FORM OF INT(3 DIGIT).
+TAKING ASCII CODES IN REVERSE AND,
+REVERSING BACK EACH INT VALUE TO ITS ORIGINAL STRING FORM.
+PRINTING ACCORDINGLY IN REVERSED ORDER.
+
+FUNCTION OF JDK LIBRARY USED ARE:
+lengthOf()
+
+ */
+
+import java.util.*;
+class rev_String
+{
+    private void main()
+    {
+        System.out.println("ENTER YOUR STATEMENT:");
+        Scanner obj= new Scanner(System.in);
+        String a = obj.nextLine();
+        String b = palindrome(a);;
+        String c;
+        String d;
+    }
+
+    String palindrome(String e)
+    {
+
+        return("");
+    }
+
+    String toText(int f)
+    {
+        return("");
+    }
+
+    String print()
+    {
+
+        return("");
+    }
+}
